2ca40026742b27bd459e1c93673e1cf939d5237c
julianParrax <julianfranco.parra@gmail.com>
2022-11-27 05:52:13 -0300

    Se agrega proyecto

---
+++ Aplication-1/Aplication1-code/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Aplication1_code
+{
+    partial class frmFormApp
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo1 = new System.Windows.Forms.Label();
+            this.btnControl1 = new System.Windows.Forms.Button();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNuevo = new System.Windows.Forms.TextBox();
+            this.lblNuevo = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo1
+            // 
+            this.lblInfo1.AutoSize = true;
+            this.lblInfo1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblInfo1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblInfo1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblInfo1.Font = new System.Drawing.Font("Imprint MT Shadow", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(14)));
+            this.lblInfo1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblInfo1.Location = new System.Drawing.Point(75, 39);
+            this.lblInfo1.Name = "lblInfo1";
+            this.lblInfo1.Size = new System.Drawing.Size(159, 25);
+            this.lblInfo1.TabIndex = 0;
+            this.lblInfo1.Text = "Apreta el boton: ";
+            this.lblInfo1.MouseLeave += new System.EventHandler(this.lblInfo1_MouseLeave);
+            this.lblInfo1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblInfo1_MouseMove);
+            // 
+            // btnControl1
+            // 
+            this.btnControl1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnControl1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnControl1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnControl1.Location = new System.Drawing.Point(238, 38);
+            this.btnControl1.Name = "btnControl1";
+            this.btnControl1.Size = new System.Drawing.Size(186, 24);
+            this.btnControl1.TabIndex = 1;
+            this.btnControl1.Text = "enviar";
+            this.btnControl1.UseVisualStyleBackColor = false;
+            this.btnControl1.Click += new System.EventHandler(this.btnControl1_Click);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(254, 106);
+            this.txtApellido.MaxLength = 15;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(170, 20);
+            this.txtApellido.TabIndex = 2;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
+            // 
+            // txtNuevo
+            // 
+            this.txtNuevo.Location = new System.Drawing.Point(254, 168);
+            this.txtNuevo.Multiline = true;
+            this.txtNuevo.Name = "txtNuevo";
+            this.txtNuevo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNuevo.Size = new System.Drawing.Size(170, 51);
+            this.txtNuevo.TabIndex = 3;
+            this.txtNuevo.Leave += new System.EventHandler(this.txtNuevo_Leave);
+            // 
+            // lblNuevo
+            // 
+            this.lblNuevo.AutoSize = true;
+            this.lblNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNuevo.Location = new System.Drawing.Point(10, 181);
+            this.lblNuevo.Name = "lblNuevo";
+            this.lblNuevo.Size = new System.Drawing.Size(238, 18);
+            this.lblNuevo.TabIndex = 4;
+            this.lblNuevo.Text = "Escribe un mensaje en el Multiline: \r\n";
+            this.lblNuevo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(113, 108);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(135, 18);
+            this.lblNumero.TabIndex = 5;
+            this.lblNumero.Text = "Ingrese un numero:";
+            // 
+            // frmFormApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(766, 367);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblNuevo);
+            this.Controls.Add(this.txtNuevo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.btnControl1);
+            this.Controls.Add(this.lblInfo1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFormApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmFormApp_FormClosed);
+            this.Load += new System.EventHandler(this.frmFormApp_Load);
+            this.Click += new System.EventHandler(this.frmFormApp_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo1;
+        private System.Windows.Forms.Button btnControl1;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNuevo;
+        private System.Windows.Forms.Label lblNuevo;
+        private System.Windows.Forms.Label lblNumero;
+    }
+}
+
---
+++ Aplication-1/Aplication1-code/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//En este proyecto:
+//1.Creamos el proyecto.
+//2. Analizamos los archivos que nos creo el Proyecto.
+//3. Cambiamos el nombre del Form1, con la propiedad Name.
+//4. Cambiamos el titulo del Formulario, con la propiedad Text.
+//5. Cambiamos el color de fondo del Formulario, con la propiedad BackColor.
+//6. Cambiamos la posición del Formulario, con la propiedad StartPosition.
+//7. Cambiamos la vista inicial del Formulario (Normal, Minimizado o Maximizado), con
+//la propiedad WindowState.
+//8. Manejar el evento Load del Formulario. 
+    //a.DobleClick sobre el Formulario carga el método del evento Load por defecto.
+    //b.Mostramos un Mensaje con MessageBox.Show.
+//9.Manejar el evento FormClosed del Formulario.
+    //a. Mostramos un Mensaje con MessageBox.Show.
+//10.No mostrar el botón de minimizar .del Formulario.
+//11.No mostrar el botón de maximizar del Formulario.
+//12.Modificar el valor de la propiedad Opacity --no causa ningun efecto por el momento.
+
+
+namespace Aplication1_code
+{
+    public partial class frmFormApp : Form
+    {
+        public frmFormApp()
+        {
+            InitializeComponent();
+        }
+
+        //8
+        private void frmFormApp_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to C#");
+        }
+
+        private void frmFormApp_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Goodbye!");
+        }
+
+        //Al dispararse el evento Click, se cambie el color del Formulario. 
+        //- Cancelamos las líneas de código anteriores y colocamos el código para cambiar el color de fondo del TextBox
+        //(Propiedad BackColor) si el TextBox se encuentra vació
+        private void btnControl1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Se disparo el evento Click", "Atención");
+            //this.BackColor = Color.Coral;
+            if (txtApellido.Text == "") //tambien podria escribirse  (txtApellido.Text.Lenght == 0)
+            {
+                txtApellido.BackColor = Color.Red;
+            }else
+                txtApellido.BackColor = SystemColors.Control;
+
+        }
+
+        //-.Manejar el evento Click .del Formulario. Determinar que botón del Mouse se pulso. 
+        //Repasar el eventArgs y la herencia y relacion a la hora de generarse un evento.
+        //parameter EventArgs = evento especificado que se va a agregar +event.
+        private void frmFormApp_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs click = (MouseEventArgs)e; //instancio la clase y accedo a sus metodos y props. Esta escrito entre llaves con
+            //la "e" al final, lo que indica que es REDUNDANTE. Revisar ese concepto. 
+            
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el botón Izquierdo", "Atención"); //el segundo parametro es el nombre del cartel emergente
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el Botón Derecho", "Atención");
+            else
+            if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Presiono el botón del Medio", "Atención");
+        }
+
+        //
+        //-Manejar el evento MouseMove del Control Label. Lo elijo desde el form1.cs[Diseño]
+        //-Cambiamos el color de la propiedad BackColor
+        //-Cambiar el estilo del cursor al dispararse el eventos MouseMove 
+        
+        private void lblInfo1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //this.BackColor = Color.Orchid; //cambia el color de la ventana: this referencia a la ventana en la que estamos trabajando.
+            lblInfo1.BackColor = Color.Orchid;
+            lblInfo1.Cursor = Cursors.Hand; //Cursors obtiene una coleccion de todos los cursores disponibles
+        }
+        //
+        //-Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta (label).
+        //-Cambiar el estilo del cursor al dispararse el evento MouseLeave.
+        private void lblInfo1_MouseLeave(object sender, EventArgs e)
+        {
+            lblInfo1.BackColor = SystemColors.Control; //devuelve el color de la apariencia del boton 3d, por eso el gris
+            lblInfo1.Cursor = Cursors.Arrow; //muy sutil 
+        }
+
+        //Manejar el evento KeyPress, para ingresar solo Números 
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e) //se puede obtener el eventArg asociado solo utilizando la e
+        {
+            //los valores que se utilizan estan relacionados con los caracteres ASCII; ESTUDIARLO
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8) //acepta tambien ';' y ':'
+                e.Handled = true;
+        }
+
+        //Manejar el evento Leave del nuevo Control TextBox.Para mostrar cuantos
+        //caracteres se ingresaron una vez que el control pierde el foco.
+        private void txtNuevo_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("El textBox tiene " + txtNuevo.Text.Length + " caracteres"); //con el tab
+        }
+    }
+}
+
+
+//con los eventos, podemos modificar PROPIEDADES del objeto propiamente dicho, pero tambien podemos ejecutar LOGICA
